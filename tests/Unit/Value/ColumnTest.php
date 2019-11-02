6ffaf7247766f6f529d9f23abb2d0f883b9c20f7
Mior Muhammad Zaki <crynobone@gmail.com>
2019-11-03 07:59:10 +0800

    Add Laravie\QueryFilter\Value\Column tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Value/ColumnTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Laravie\QueryFilter\Tests\Unit\Value;
+
+use PHPUnit\Framework\TestCase;
+use Laravie\QueryFilter\Value\Column;
+use Illuminate\Database\Query\Expression;
+
+class ColumnTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider validColumnNameDataProvider
+     */
+    public function it_can_validate_column_name($given)
+    {
+        $this->assertTrue((new Column($given))->validate());
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidColumnNameDataProvider
+     */
+    public function it_cant_validate_invalid_column_name($given)
+    {
+        $this->assertFalse((new Column($given))->validate());
+    }
+
+    /**
+     * Valid column name data provider.
+     *
+     * @return array
+     */
+    public function validColumnNameDataProvider()
+    {
+        return [
+            [new Expression('users.fullname')],
+            ['fullname'],
+            [\str_pad('email', 64, 'x')],
+        ];
+    }
+
+    /**
+     * Valid column name data provider.
+     *
+     * @return array
+     */
+    public function invalidColumnNameDataProvider()
+    {
+        return [
+            ['email->"%27))%23injectedSQL'],
+            [\str_pad('email', 65, 'x')],
+            [''],
+            [null],
+        ];
+    }
+}
