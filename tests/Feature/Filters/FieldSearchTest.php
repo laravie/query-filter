21f43d52efed4a660659f226f42ae8a3ca4d0538
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-30 09:22:41 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Filters/FieldSearchTest.php
+++ tests/Feature/Filters/FieldSearchTest.php
@@ -2,11 +2,13 @@
 
 namespace Laravie\QueryFilter\Tests\Feature\Filters;
 
+use Illuminate\Database\Eloquent\Builder as EloquentQueryBuilder;
 use Laravie\QueryFilter\Filters\FieldSearch;
 use Laravie\QueryFilter\Searchable;
 use Laravie\QueryFilter\Tests\Factories\UserFactory;
 use Laravie\QueryFilter\Tests\Models\User;
 use Laravie\QueryFilter\Tests\TestCase;
+use Mockery as m;
 
 class FieldSearchTest extends TestCase
 {
@@ -39,5 +41,26 @@ class FieldSearchTest extends TestCase
         );
 
         $this->assertSame(5, $query->count());
+    }
+
+    /** @test */
+    public function it_can_build_search_query_with_empty_keyword()
+    {
+        UserFactory::new()->times(5)->create([
+            'name' => 'hello world',
+        ]);
+
+        UserFactory::new()->times(3)->create([
+            'name' => 'goodbye world',
+        ]);
+
+        $stub = new FieldSearch('name');
+
+        $query = m::spy(EloquentQueryBuilder::class);
+
+        $query->shouldNotHaveReceived('orWhere');
+
+        $stub->apply($query, [''], 'like', 'orWhere');
+
     }
 }
