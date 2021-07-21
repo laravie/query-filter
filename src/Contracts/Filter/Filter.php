dd7a1c0368f0f4a4aa5d62a39b1f9060921b100b
Mior Muhammad Zaki <crynobone@gmail.com>
2021-07-21 10:48:15 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Contracts/Filter/Filter.php
+++ src/Contracts/Filter/Filter.php
@@ -11,7 +11,6 @@ interface Filter
      * Apply search queries.
      *
      * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
-     * @param  array  $keywords
      *
      * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
      */
