a1f2dcd161dfdd12804d827a6eaf4bf46e8b6dd0
Mior Muhammad Zaki <crynobone@gmail.com>
2021-07-27 20:06:32 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Contracts/Filter/Filter.php
+++ src/Contracts/Filter/Filter.php
@@ -10,9 +10,9 @@ interface Filter
     /**
      * Apply search queries.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
+     * @param  \Illuminate\Contracts\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
      *
-     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
+     * @return \Illuminate\Contracts\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
      */
     public function apply($query, array $keywords, string $likeOperator, string $whereOperator);
 }
